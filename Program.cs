9df4c1279bd95321f0849a550ef3ba2abfbbf6d0
Tristan Postley <tpostley@gmail.com>
2021-12-07 10:11:34 -0800

    Initial

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueFractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            double total = 0;
+            List<double> temp = new List<double>();
+
+            for(double d = 1; d < 10; d++)
+            {
+                for(double n = 1; n < 9; n++)
+                {
+                    double fraction = n/d;
+                    if(fraction >= 1)
+                    {
+                        break;
+                    }
+                    if(temp.Contains(fraction))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        temp.Add(fraction);
+                        total += fraction;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Total of all unique fractions less than 1 whose numerator and denominator are single digit: {total}");
+        }
+    }
+}
